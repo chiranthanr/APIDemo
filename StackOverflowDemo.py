735f24afacc11736d06d6369aa63b4d72cc6e895
Chiranthan Ram <chiranthan@gmail.com>
2024-04-13 20:39:03 +0530

    Stack Overflow API connection demo

--- StackOverflowDemo.py
+++ StackOverflowDemo.py
@@ -1,2 +1,10 @@
 import requests
-import json
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    result = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+    for data in result.json()['items']:
+       print(data['title'])
+    return 'This is the output from the API'
